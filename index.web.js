38e4d587ec1e890f1f9a4a76c05c1bbb82475aa7
James Baxley <james.baxley@newspring.cc>
2017-03-22 13:04:30 -0400

    add windows support

--- index.web.js
+++ index.web.js
@@ -3,10 +3,10 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import {AppContainer} from 'react-hot-loader';
-import dune from './src';
+import React from "react";
+import { AppRegistry } from "react-native";
+import { AppContainer } from "react-hot-loader";
+import dune from "./src";
 
 const render = Component => {
   const App = () => (
@@ -14,10 +14,10 @@ const render = Component => {
       <Component />
     </AppContainer>
   );
-  AppRegistry.registerComponent('dune', () => App);
-  AppRegistry.runApplication('dune', {
+  AppRegistry.registerComponent("dune", () => App);
+  AppRegistry.runApplication("dune", {
     initialProps: {},
-    rootTag: document.getElementById('react-app'),
+    rootTag: document.getElementById("react-app"),
   });
 };
 
@@ -25,7 +25,8 @@ render(dune);
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./src', () => {
-    render(dune);
+  module.hot.accept("./src", () => {
+    const newDune = require("./src").default;
+    render(newDune);
   });
 }
