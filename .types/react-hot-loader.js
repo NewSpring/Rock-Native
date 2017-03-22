909f9464fdcd8d462389b0cbef9797b5ecd6191d
richarddubay <richardtdubayjr@gmail.com>
2017-03-22 15:34:27 -0400

    added flow typing

---
+++ .types/react-hot-loader.js
@@ -0,0 +1,9 @@
+declare module 'react-hot-loader' {
+  declare function AppContainer(): Function;
+}
+
+declare var module: {
+  hot: {
+    accept(path: string, callback: () => void): void,
+  },
+};
---
+++ .types/react-native.js
@@ -0,0 +1,6 @@
+declare module 'react-native' {
+  declare function AppRegistry(): Function;
+  declare function StyleSheet(): Function;
+  declare function Text(): Function;
+  declare function View(): Function;
+}
