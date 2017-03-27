ebbe43ff2a6431602b5618d7598f204ef2aa2864
James Baxley <james.baxley@newspring.cc>
2017-03-27 15:10:24 -0400

    DEBUG

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
-
+echo 'hello world'
+echo $(which now)
 # deploy to now
-URL=$(now ./web/client/dist -e NODE_ENV=production --token ${NOW_TOKEN})
+# URL=$(now ./web/client/dist -e NODE_ENV=production --token ${NOW_TOKEN})
 # alias to now
-now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_BRANCH}.now.sh
+# now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_BRANCH}.now.sh
