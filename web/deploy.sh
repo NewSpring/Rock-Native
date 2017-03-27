38bb75d3fa90ef8420757175a17dd7af4079526b
James Baxley <james.baxley@newspring.cc>
2017-03-27 15:28:00 -0400

    log to github as status

--- web/deploy.sh
+++ web/deploy.sh
@@ -3,3 +3,5 @@
 URL=$(now ./web/client/dist -e NODE_ENV=production --token ${NOW_TOKEN})
 # alias to now
 now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_BRANCH}.now.sh
+# respond to github with link
+commit-status "success" "web-deploy" "PR deployed succesfully!" ${URL}
