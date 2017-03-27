2b4a66da78bae61c7b17e43795527247edc6d992
James Baxley <james.baxley@newspring.cc>
2017-03-27 15:33:31 -0400

    use newspring.io

--- web/deploy.sh
+++ web/deploy.sh
@@ -2,6 +2,6 @@
 # deploy to now
 URL=$(now ./web/client/dist -e NODE_ENV=production --token ${NOW_TOKEN})
 # alias to now
-now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_BRANCH}.now.sh
+now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_BRANCH}.newspring.io
 # respond to github with link
-commit-status "success" "web-deploy" "PR deployed succesfully!" ${URL}
+commit-status "success" "web-deploy" "PR deployed succesfully!" ${TRAVIS_BRANCH}.newspring.io
