9384ed4fb619e149db7cbd923f6cdfdaf6de2fcc
James Baxley <james.baxley@newspring.cc>
2017-03-27 15:16:35 -0400

    fix path and script execute

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
-echo 'hello world'
-echo $(which now)
 # deploy to now
-# URL=$(now ./web/client/dist -e NODE_ENV=production --token ${NOW_TOKEN})
+URL=$(now ./web/client/dist -e NODE_ENV=production --token ${NOW_TOKEN})
 # alias to now
-# now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_BRANCH}.now.sh
+now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_BRANCH}.now.sh
