66fab49b6b8ae0cf9a4939c468f804885740ebbd
James Baxley <james.baxley@newspring.cc>
2017-03-27 15:04:25 -0400

    try travis branch

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
+
 # deploy to now
 URL=$(now ./web/client/dist -e NODE_ENV=production --token ${NOW_TOKEN})
 # alias to now
-now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_PULL_REQUEST_BRANCH}.now.sh
+now --token ${NOW_TOKEN} alias set ${URL} ${TRAVIS_BRANCH}.now.sh
