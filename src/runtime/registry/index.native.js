72a8d12bab44bd26dc9cc08989a7b8c9ba02a8d6
Jake Dawkins <dawkinsjh@gmail.com>
2017-05-03 15:28:29 -0400

    added glob imports for native registry

--- src/runtime/registry/index.native.js
+++ src/runtime/registry/index.native.js
@@ -4,20 +4,14 @@ import type, { Component } from "react";
 import Junction from "../../junction";
 import { mapProps } from "recompose";
 
-import HelloWorld from "../../blocks/HelloWorld";
-import Counter from "../../blocks/Counter";
-
-const available = {
-  HelloWorld: HelloWorld,
-  Counter: Counter,
-};
+import * as availableBlocks from "glob:../../blocks/*";
 
 export const load = mapProps(({ ...rest, registry }) => ({
   ...rest,
   components: registry.blocks.map(({ ...details, path }) => ({
     ...details,
     path,
-    Component: available[path],
+    Component: availableBlocks[path],
   })),
 }));
 
