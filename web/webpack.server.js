536091cf44d9fd11b23a1b1d16b751a8e66eb9a8
James Baxley <james.baxley@newspring.cc>
2017-05-03 22:32:37 -0400

    fix server builds for the ci to run

--- web/webpack.server.js
+++ web/webpack.server.js
@@ -1,46 +1,54 @@
 const path = require("path");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ZipPlugin = require('zip-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
 
 const sharedConfig = require("./webpack.base");
 
 module.exports = env => {
   const isDevBuild = !(env && env.prod);
-  
+
   // Configuration for server-side (prerendering) bundle suitable for running in Node
   const serverBundleConfig = merge(sharedConfig(isDevBuild), {
     resolve: { mainFields: ["main"], extensions: [".server.js"] },
-    entry: { server: ["regenerator-runtime/runtime", path.join(__dirname, "..", "./index.server.js")] },
+    entry: {
+      server: [
+        "regenerator-runtime/runtime",
+        path.join(__dirname, "..", "./index.server.js"),
+      ],
+    },
     plugins: [
+      new webpack.ContextReplacementPlugin(/blocks/, /^index\.js/, false),
       new webpack.DllReferencePlugin({
         context: __dirname,
-        manifest: require(
-          path.join(
-            __dirname,
-            "dist",
-            "server",
-            "vendor-manifest.json"
-          )
-        ),
+        manifest: require(path.join(
+          __dirname,
+          "dist",
+          "server",
+          "vendor-manifest.json"
+        )),
         sourceType: "commonjs2",
         name: "./vendor",
       }),
       new CopyWebpackPlugin([
-          { from: path.join(__dirname, "handler.js"), to: path.join(__dirname, "dist", "server") },
-          { from: path.join(__dirname, "dist", "server", "vendor.js") },
-      ])
-    ].concat(isDevBuild ? [
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.NamedModulesPlugin(),
-    ]: [
-      new ZipPlugin({
-        path: './../../',
-        filename: 'dist.zip',
-        include: [/\.js$/],
-      })
-    ]),
+        {
+          from: path.join(__dirname, "handler.js"),
+          to: path.join(__dirname, "dist", "server"),
+        },
+        { from: path.join(__dirname, "dist", "server", "vendor.js") },
+      ]),
+    ].concat(
+      isDevBuild
+        ? [new webpack.NoEmitOnErrorsPlugin(), new webpack.NamedModulesPlugin()]
+        : [
+            new ZipPlugin({
+              path: "./../../",
+              filename: "dist.zip",
+              include: [/\.js$/],
+            }),
+          ]
+    ),
     output: {
       libraryTarget: "commonjs",
       library: "RockNative",
