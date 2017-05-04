23b6efeb54391a457e4a357c43a6ad2f2b1aac2b
James Baxley <james.baxley@newspring.cc>
2017-05-03 22:50:52 -0400

    simplify how webpack will create server and just use native version

--- src/runtime/registry/index.js
+++ src/runtime/registry/index.js
@@ -2,25 +2,17 @@
 
 import { mapProps } from "recompose";
 import Junction from "../../junction";
-import type { IRegistryRequest } from "./util/types";
 
 // $FlowIgnore
 import * as availableBlocks from "glob:../../blocks/*";
 
-export const mapper = ({
-  ...rest,
-  registry,
-}: {
-  registry: IRegistryRequest,
-}) => ({
+export const load = mapProps(({ ...rest, registry }) => ({
   ...rest,
   components: registry.blocks.map(({ ...details, path }) => ({
     ...details,
     path,
     Component: availableBlocks[path],
   })),
-});
-
-export const load = mapProps(mapper);
+}));
 
 export default Junction().with(load);
