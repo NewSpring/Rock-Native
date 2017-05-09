988dc526ff17ccdf037f9d00791adf288da5ccfb
Jake Dawkins <dawkinsjh@gmail.com>
2017-05-09 11:54:39 -0400

    tested index with new loader

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,7 +1,12 @@
 import { shallow } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
 import { defaultProps } from "recompose";
-import Comps, { layoutStyle } from "../index.js";
+import Comps, {
+  layoutStyle,
+  loadingState,
+  loadingCheck,
+  Loading,
+} from "../index.js";
 
 const registry = {
   blocks: [{ path: "HelloWorld", id: 2 }, { path: "Counter", id: 1 }],
@@ -19,9 +24,35 @@ describe("Layout Style", () => {
 });
 
 describe("Index", () => {
+  // XXX figure out how to test this with react-router
   it("should render the components", () => {
     const tree = shallow(generateComponent());
     expect(tree).toBeDefined();
     expect(shallowToJson(tree)).toMatchSnapshot();
+  });
+});
+
+describe("loadingCheck", () => {
+  it("should be false if not explicitly said to be loading", () => {
+    expect(loadingCheck()).toEqual(false);
+  });
+  it("should be true if loading key is true", () => {
+    expect(loadingCheck({ loading: true })).toEqual(true);
+  });
+});
+
+describe("Loading", () => {
+  // XXX UPDATE WHEN WE HAVE A REAL ONE
+  it("should render loading state (null)", () => {
+    const component = shallow(<Loading />);
+    expect(component.html()).toEqual(null);
+  });
+});
+
+describe("loadingState", () => {
+  it("is a recompose hoc", () => {
+    const state = loadingState(() => () => null);
+    expect(typeof state).toEqual("function");
+    expect(state.displayName).toEqual("branch(Component)");
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // @flow
 
 import Style from "@jongold/further";
+// import { ifElse } from "ramda";
+import { branch } from "recompose";
+import { withRouter } from "react-router";
 import Junction from "./junction";
 import loadBlocks from "./runtime/registry";
 import Layout from "./layout";
@@ -10,9 +13,15 @@ export const layoutStyle = Style.of({
   flex: 1,
 });
 
+export const loadingCheck = ({ loading } = { loading: false }) => loading;
+export const Loading = () => null;
+export const loadingState = branch(loadingCheck, () => Loading);
+
 export default Junction()
-  // XXX load data from graphql
-  .with(loadRouteData)
+  .with(withRouter)
+  // .with(mapProps(props => (console.log(props), props)))
+  .with(loadRouteData) // load data from graphql
+  .with(loadingState)
   .with(loadBlocks)
   // XXX load layout file
   // XXX place blocks in layout
