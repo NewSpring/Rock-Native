3d7c4c13f44f7c9d0abd6a6186d31f1b5c51612d
richarddubay <richardtdubayjr@gmail.com>
2017-05-09 16:27:51 -0400

    added the display name for native

--- src/blocks/HelloWorld/helloWorld.native.js
+++ src/blocks/HelloWorld/helloWorld.native.js
@@ -1,6 +1,10 @@
-stdin: SyntaxError: Duplicate export for `default` (12:0)
-  10 | );
-  11 | 
-> 12 | export default HelloWorld;
-     | ^
-  13 | 
+// @flow
+import { Text } from "react-native";
+import { helloStyle } from "./";
+
+export const HelloWorld = () => (
+  <Text style={helloStyle.resolve()}>Hello World</Text>
+);
+HelloWorld.displayName = "HelloWorld";
+
+export default HelloWorld;
