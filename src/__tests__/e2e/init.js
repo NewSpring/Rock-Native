fd927476b8fb12605403bfa7f5cec225dcab731f
Jake Dawkins <dawkinsjh@gmail.com>
2017-06-08 10:33:53 -0400

    moved e2e tests to src/__tests__

--- src/__tests__/e2e/init.js
+++ src/__tests__/e2e/init.js
@@ -2,7 +2,7 @@
 
 require("babel-polyfill");
 const detox = require("detox");
-const config = require("../package.json").detox;
+const config = require("../../../package.json").detox;
 
 before(async () => {
   await detox.init(config);
