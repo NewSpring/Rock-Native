8f66a2450eb5742d4996bbc8e84512e9599d1639
richarddubay <richardtdubayjr@gmail.com>
2017-06-12 10:44:43 -0400

    added a recompose mock file

---
+++ __mocks__/recompose.js
@@ -0,0 +1,14 @@
+// This should be able to be called from tests but currently
+// it is not working.
+
+// import { curry, fromPairs, map, adjust, toPairs } from "ramda";
+// import R from "recompose";
+//
+// const mapValues = curry((fn, obj) =>
+//   fromPairs(map(adjust(fn, 1), toPairs(obj))),
+// );
+//
+// const withJest = mapValues(fn => jest.fn(fn));
+//
+console.log("hai");
+// export default withJest(R);
